2580430df0d203369e7c0a16426eb044622c6b6e
Manfred Touron <94029+moul@users.noreply.github.com>
2021-06-29 13:34:52 +0200

    chore: setup CLI

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module moul.io/midcat
 go 1.13
 
 require (
+	github.com/peterbourgon/ff/v3 v3.0.0
 	github.com/tailscale/depaware v0.0.0-20201214215404-77d1e9757027
 	go.uber.org/goleak v1.1.10
-	go.uber.org/zap v1.17.0 // indirect
+	go.uber.org/zap v1.17.0
 	moul.io/motd v1.0.0
 	moul.io/srand v1.6.1
-	moul.io/u v1.24.0
+	moul.io/u v1.24.0 // indirect
 	moul.io/zapconfig v1.4.0
 )
--- main.go
+++ main.go
@@ -1,31 +1,78 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
 
+	"github.com/peterbourgon/ff/v3/ffcli"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"moul.io/motd"
 	"moul.io/srand"
-	"moul.io/u"
 	"moul.io/zapconfig"
 )
 
 func main() {
-	if err := run(os.Args); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	if err := run(os.Args[1:]); err != nil {
+		if !errors.Is(err, flag.ErrHelp) {
+			fmt.Fprintf(os.Stderr, "error: %+v\n", err)
+		}
 		os.Exit(1)
 	}
 }
 
+var opts struct {
+	debug  bool
+	logger *zap.Logger
+}
+
 func run(args []string) error {
-	rand.Seed(srand.Fast())
-	fmt.Print(motd.Default())
-	logger, err := zapconfig.Configurator{}.Build()
-	if err != nil {
-		return err
+	// setup CLI
+	rootFs := flag.NewFlagSet("midcat", flag.ExitOnError)
+	rootFs.BoolVar(&opts.debug, "debug", opts.debug, "debug mode")
+	root := &ffcli.Command{
+		Name:       "midcat",
+		FlagSet:    rootFs,
+		ShortUsage: "midcat [options] <address> <address>",
+		Exec:       doRoot,
 	}
-	logger.Info("Hello World!")
-	fmt.Println("args", u.JSON(args))
+	if err := root.Parse(args); err != nil {
+		return fmt.Errorf("parse error: %w", err)
+	}
+
+	// init logger
+	{
+		rand.Seed(srand.Fast())
+		config := zapconfig.New().SetPreset("light-console")
+		if opts.debug {
+			config = config.SetLevel(zapcore.DebugLevel)
+
+		} else {
+			config = config.SetLevel(zapcore.InfoLevel)
+		}
+		var err error
+		opts.logger, err = config.Build()
+		if err != nil {
+			return fmt.Errorf("logger init: %w", err)
+		}
+	}
+
+	// run
+	{
+		if err := root.Run(context.Background()); err != nil {
+			return fmt.Errorf("run error: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func doRoot(ctx context.Context, args []string) error {
+	opts.logger.Debug("args", zap.Strings("args", args))
+	fmt.Print(motd.Default())
 	return nil
 }
