caef377515a3b3283030588f7b4d11ac062b29cd
Manfred Touron <94029+moul@users.noreply.github.com>
2021-06-29 15:46:57 +0200

    chore: custom usage
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.13
 require (
 	github.com/peterbourgon/ff/v3 v3.0.0
 	github.com/tailscale/depaware v0.0.0-20201214215404-77d1e9757027
+	gitlab.com/gomidi/midi v1.23.5
+	gitlab.com/gomidi/portmididrv v0.7.0
 	go.uber.org/goleak v1.1.10
+	go.uber.org/multierr v1.7.0
 	go.uber.org/zap v1.17.0
 	moul.io/motd v1.0.0
 	moul.io/srand v1.6.1
--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strings"
 
 	"github.com/peterbourgon/ff/v3/ffcli"
+	"gitlab.com/gomidi/midi"
+	"gitlab.com/gomidi/midi/writer"
+	driver "gitlab.com/gomidi/portmididrv"
+	"go.uber.org/multierr"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"moul.io/motd"
@@ -26,8 +31,9 @@ func main() {
 }
 
 var opts struct {
-	debug  bool
-	logger *zap.Logger
+	debug      bool
+	logger     *zap.Logger
+	midiDriver midi.Driver
 }
 
 func run(args []string) error {
@@ -37,8 +43,9 @@ func run(args []string) error {
 	root := &ffcli.Command{
 		Name:       "midcat",
 		FlagSet:    rootFs,
-		ShortUsage: "midcat [options] <address> <address>",
+		ShortUsage: "midcat [FLAGS] <ADDRESS>[,OPTS] <ADDRESS>[,OPTS]",
 		Exec:       doRoot,
+		UsageFunc:  usage,
 	}
 	if err := root.Parse(args); err != nil {
 		return fmt.Errorf("parse error: %w", err)
@@ -50,7 +57,6 @@ func run(args []string) error {
 		config := zapconfig.New().SetPreset("light-console")
 		if opts.debug {
 			config = config.SetLevel(zapcore.DebugLevel)
-
 		} else {
 			config = config.SetLevel(zapcore.InfoLevel)
 		}
@@ -71,8 +77,64 @@ func run(args []string) error {
 	return nil
 }
 
+func usage(c *ffcli.Command) string {
+	var b strings.Builder
+
+	fmt.Fprint(&b, ffcli.DefaultUsageFunc(c)+"\n\n")
+
+	fmt.Fprintf(&b, "ADDRESS\n")
+	fmt.Fprintf(&b, "  -           stdio\n")
+	fmt.Fprintf(&b, "  pipe        echo/fifo\n")
+	fmt.Fprintf(&b, "  tcp         ...\n")
+	fmt.Fprintf(&b, "  tick        ...\n")
+	fmt.Fprintf(&b, "  rand        ...\n")
+	fmt.Fprintf(&b, "  udp         ...\n")
+	fmt.Fprintf(&b, "  websocket   ...\n")
+	fmt.Fprintf(&b, "\n")
+
+	fmt.Fprintf(&b, "OPTS\n")
+	fmt.Fprintf(&b, "  debug       ...\n")
+	fmt.Fprintf(&b, "  reconnect   ...\n")
+	fmt.Fprintf(&b, "  bpm         ...\n")
+	fmt.Fprintf(&b, "  quantify    ...\n")
+	fmt.Fprintf(&b, "  filter      ...\n")
+	fmt.Fprintf(&b, "\n")
+
+	fmt.Fprintf(&b, "HARDWARE\n")
+	drv, err := driver.New()
+	if err != nil {
+		fmt.Fprintf(&b, "  error: %v\n", err)
+	} else {
+		var errs error
+		ins, err := drv.Ins()
+		if err != nil {
+			errs = multierr.Append(errs, err)
+		} else {
+			for _, in := range ins {
+				fmt.Fprintf(&b, "  IN:  number=%d is-open=%v name=%q\n", in.Number(), in.IsOpen(), in.String())
+			}
+		}
+
+		outs, err := drv.Outs()
+		if err != nil {
+			errs = multierr.Append(errs, err)
+		} else {
+			for _, out := range outs {
+				fmt.Fprintf(&b, "  OUT: number=%d is-open=%v name=%q\n", out.Number(), out.IsOpen(), out.String())
+			}
+		}
+
+		if errs != nil {
+			fmt.Fprintf(&b, "error: %v\n", errs)
+		}
+	}
+
+	return strings.TrimSpace(b.String())
+}
+
 func doRoot(ctx context.Context, args []string) error {
 	opts.logger.Debug("args", zap.Strings("args", args))
 	fmt.Print(motd.Default())
+	_ = writer.New
 	return nil
 }
